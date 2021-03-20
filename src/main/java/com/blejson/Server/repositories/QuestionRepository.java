cfa3369676e53a27126a2c18c0256aceb0810a23
Jacek B <jaceble@interia.eu>
2021-03-20 19:42:34 +0100

    repositories added

--- src/main/java/com/blejson/Server/repositories/QuestionRepository.java
+++ src/main/java/com/blejson/Server/repositories/QuestionRepository.java
@@ -1,2 +1,17 @@
-package com.blejson.Server.repositories;public interface QuestionRepository {
+package com.blejson.Server.repositories;
+
+import com.blejson.Server.entity.Question;
+import com.blejson.Server.entity.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+    Optional<Question> findById(Long id);
+    List<Question> findByQuiz(Quiz quiz);
+    List<Question> findAll();
+    void delete(Question question);
 }
--- src/main/java/com/blejson/Server/repositories/QuizRepository.java
+++ src/main/java/com/blejson/Server/repositories/QuizRepository.java
@@ -1,2 +1,17 @@
-package com.blejson.Server.repositories;public class QuizRepository {
+package com.blejson.Server.repositories;
+
+import com.blejson.Server.entity.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface QuizRepository extends JpaRepository<Quiz, String> {
+    Optional<Quiz> findById(String id);
+    List<Quiz> findAll();
+    void deleteById(String id);
+    void delete(Quiz quiz);
+
 }
